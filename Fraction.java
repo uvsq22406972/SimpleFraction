52cb7748fd914a9de3eaf075e52bf5c9dc45d880
22406972 <naufal.somantri@ens.uvsq.fr>
2024-09-18 15:02:32 +0200

    Création Fraction.java et Main.java

---
+++ Fraction.java
@@ -0,0 +1,7 @@
+package SimpleFraction;
+
+public class Fraction{
+    public String toString(){
+        return "Je suis une fraction";
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+package SimpleFraction;
+
+public class Main {
+    public static void man (String[] args){
+        System.out.println("Hello World");
+    }
+}
