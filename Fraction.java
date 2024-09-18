65ad8310f7cd620f93bf0165d83794b53d7a59a8
22406972 <naufal.somantri@ens.uvsq.fr>
2024-09-18 17:01:33 +0200

    Fin du TD1

--- Fraction.java
+++ Fraction.java
@@ -1,7 +1,69 @@
-package SimpleFraction;
-
 public class Fraction{
+    //Déclaration des variables
+    private int numerateur,denominateur;
+    
+    //Déclaration des constantes
+    public static final Fraction ZERO = new Fraction(0,1);
+    public static final Fraction ONE = new Fraction (1,1);
+
+    //Constructeurs
+    //Constructeur avec nominateur et dénominateur
+    public Fraction(int numerateur, int denominateur){
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+    //Constructeur avec nominateur
+    public Fraction (int numerateur){
+        this.numerateur = numerateur;
+        this.denominateur = 1;
+    }
+
+    //Constructeur sans argument
+    public Fraction (){
+        this.numerateur = 0;
+        this.denominateur = 1;
+    }
+    //Méthode
+    //Nombre à virgule flottante
+    public double FractionToFlottant(){
+        return (double) this.numerateur / (double) this.denominateur;
+    }
+
+    //Addition des fractions
+    public Fraction AddFraction (Fraction f){
+        return new Fraction(f.numerateur*this.denominateur + this.numerateur*f.denominateur, f.denominateur * this.denominateur);
+    }
+
+    //Simplification des fractions
+    public int pgcd(int num, int denom) {
+        while(num != denom){
+            if(num > denom)
+                num = num - denom;
+            else
+                denom = denom - num;
+        }
+        return denom;
+    }
+
+    public void SimplifyFraction(){
+        int pgcd = pgcd(this.numerateur, this.denominateur);
+        this.numerateur = this.numerateur/pgcd;
+        this.denominateur = this.denominateur/pgcd;
+
+    }
+
+    //Accesseur
+    //Numérateur
+    public int getNumerateur(){
+        return this.numerateur;
+    }
+    //Dénominateur
+    public int getDenominateur(){
+        return this.denominateur;
+    }
+
+    //Affichage
     public String toString(){
-        return "Je suis une fraction";
+        return "Je suis une fraction (" + this.numerateur + "/" + this.denominateur + ")";
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,32 @@
-package SimpleFraction;
-
 public class Main {
-    public static void man (String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args){
+        //Objet en utilisant le constructeur numérateur et dénominateur
+        Fraction f1 = new Fraction(2,3);
+        System.out.println(f1.toString());
+
+        //Objet en utilisant le constructeur numérateur
+        Fraction f2 = new Fraction(5);
+        System.out.println(f2.toString());
+
+        //Objet en utilisant le constructeur sans argument
+        Fraction f3 = new Fraction();
+        System.out.println(f3.toString());
+
+        //Méthode FractionToFlottant
+        System.out.println(String.format("%.3f", f1.FractionToFlottant()));
+
+        //Méthode AddFraction
+        Fraction f4 = f1.AddFraction(f2);
+        System.out.println(f4.toString());
+
+        //Methode SimplifyFraction
+        Fraction f5 = new Fraction(15,12);
+        System.out.println(f5.pgcd(15,12));
+        f5.SimplifyFraction();
+        System.out.println(f5.toString());
+
+        //
+        Fraction f6 = new Fraction(5,4);
+        System.out.println(f5 == f6);
     }
 }
