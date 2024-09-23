7d468bca4c7cf5056623d1135697e26f3247f77a
22406972 <naufal.somantri@ens.uvsq.fr>
2024-09-23 14:59:30 +0200

    Fin du TD1

--- Fraction.java
+++ Fraction.java
@@ -1,4 +1,4 @@
-public class Fraction{
+public class Fraction extends Number implements Comparable<Fraction>{
     //Déclaration des variables
     private int numerateur,denominateur;
     
@@ -7,12 +7,12 @@ public class Fraction{
     public static final Fraction ONE = new Fraction (1,1);
 
     //Constructeurs
-    //Constructeur avec nominateur et dénominateur
+    //Constructeur avec numérateur et dénominateur
     public Fraction(int numerateur, int denominateur){
         this.numerateur = numerateur;
         this.denominateur = denominateur;
     }
-    //Constructeur avec nominateur
+    //Constructeur avec numérateur
     public Fraction (int numerateur){
         this.numerateur = numerateur;
         this.denominateur = 1;
@@ -31,6 +31,9 @@ public class Fraction{
 
     //Addition des fractions
     public Fraction AddFraction (Fraction f){
+        if (this.denominateur == f.denominateur)
+            return new Fraction(this.numerateur + f.numerateur, this.denominateur);
+
         return new Fraction(f.numerateur*this.denominateur + this.numerateur*f.denominateur, f.denominateur * this.denominateur);
     }
 
@@ -38,9 +41,9 @@ public class Fraction{
     public int pgcd(int num, int denom) {
         while(num != denom){
             if(num > denom)
-                num = num - denom;
+            num = num - denom;
             else
-                denom = denom - num;
+            denom = denom - num;
         }
         return denom;
     }
@@ -49,8 +52,40 @@ public class Fraction{
         int pgcd = pgcd(this.numerateur, this.denominateur);
         this.numerateur = this.numerateur/pgcd;
         this.denominateur = this.denominateur/pgcd;
-
     }
+
+    //Override equals (Renvoie true lorsque le contenu est égal)
+    @Override
+    public boolean equals(Object object) {
+    Fraction autreFraction= (Fraction) object;
+        if (this.numerateur != autreFraction.numerateur)
+            return false;
+        if (this.denominateur != autreFraction.denominateur)
+            return false;
+        return true;
+    }
+
+    //Même dénominateur
+    public int ppcm(Fraction f){
+        int denom_f1 = this.denominateur;
+        int denom_f2 = f.denominateur;
+        return (denom_f1 * denom_f2) / pgcd(denom_f1, denom_f2);
+    }
+
+    public int compareTo(Fraction f) {
+        //0 définit égale
+        //1 définit la droite est plus grande que la gauche
+        //-1 définit la droite est plus petite que la gauche
+
+        int ppcm = ppcm(f);
+        int compare_f1 = this.numerateur * (ppcm / this.denominateur);
+        int compare_f2 = f.numerateur * (ppcm / f.denominateur);
+
+        if (compare_f1 == compare_f2) return 0;
+        if (compare_f1 > compare_f2) return 1;
+
+        return -1; 
+    }  
 
     //Accesseur
     //Numérateur
@@ -63,7 +98,43 @@ public class Fraction{
     }
 
     //Affichage
+    public String ShowFraction(){
+        return this.numerateur + "/" + this.denominateur;
+    }
+
+    @Override
     public String toString(){
         return "Je suis une fraction (" + this.numerateur + "/" + this.denominateur + ")";
+    }
+
+    //Héritage de la classe Number
+    @Override
+    public byte byteValue() {
+        return (byte) FractionToFlottant();
+    }   
+
+    @Override
+    public double doubleValue() {
+        return FractionToFlottant();
+    }
+
+    @Override
+    public float floatValue() {
+        return (float) FractionToFlottant();
+    }
+
+    @Override
+    public int intValue() {
+        return (int) FractionToFlottant();
+    }
+ 
+    @Override
+    public long longValue() {
+        return (long) FractionToFlottant();
+    }
+
+    @Override
+    public short shortValue() {
+        return (short) FractionToFlottant();
     }
 }
--- Main.java
+++ Main.java
@@ -1,32 +1,128 @@
 public class Main {
     public static void main(String[] args){
+        //JEU DE TESTS À LA FIN DU MAIN
+
         //Objet en utilisant le constructeur numérateur et dénominateur
         Fraction f1 = new Fraction(2,3);
-        System.out.println(f1.toString());
+
+        //System.out.println(f1.toString());
+        /*
+        System.out.println("####################");
+        System.out.println("Fraction f1 :");
+        System.out.println(f1.ShowFraction());
+        */
 
         //Objet en utilisant le constructeur numérateur
         Fraction f2 = new Fraction(5);
-        System.out.println(f2.toString());
+        //System.out.println(f2.toString());
+        /*
+        System.out.println("####################");
+        System.out.println("Fraction f2 :");
+        System.out.println(f2.ShowFraction());
+        */
 
         //Objet en utilisant le constructeur sans argument
         Fraction f3 = new Fraction();
-        System.out.println(f3.toString());
+        //System.out.println(f3.toString());
+        /*
+        System.out.println("####################");
+        System.out.println("Fraction f3 :");
+        System.out.println(f3.ShowFraction());
+        */
 
         //Méthode FractionToFlottant
+        /*
+        System.out.println("Fraction f3 en décimal :");
         System.out.println(String.format("%.3f", f1.FractionToFlottant()));
+        */
 
         //Méthode AddFraction
         Fraction f4 = f1.AddFraction(f2);
-        System.out.println(f4.toString());
+        //System.out.println(f4.toString());
+        /*
+        System.out.println("####################");
+        System.out.println("Fraction f4 :");
+        System.out.println(f4.ShowFraction());
+        */
 
         //Methode SimplifyFraction
         Fraction f5 = new Fraction(15,12);
+        Fraction f6 = new Fraction(105,84);
+        /*
+        System.out.println("####################");
+        System.out.println("Fraction f5 :");
+        System.out.println(f5.ShowFraction());
+        System.out.println("PGCD f5 :");
         System.out.println(f5.pgcd(15,12));
+        System.out.println("Fraction f5 simplifié :");
         f5.SimplifyFraction();
-        System.out.println(f5.toString());
+        System.out.println(f5.ShowFraction());
+        System.out.println("####################");
+        System.out.println("Fraction f6 :");
+        System.out.println(f6.ShowFraction());
+        System.out.println("PGCD f6 :");
+        System.out.println(f6.pgcd(105,84));
+        System.out.println("Fraction f6 simplifié :");
+        f6.SimplifyFraction();
+        System.out.println(f6.ShowFraction());
+        */
 
-        //
-        Fraction f6 = new Fraction(5,4);
-        System.out.println(f5 == f6);
+        //Test d'égalité (En comparant en forme de fraction, ça aurait été plus simple si on fait une comparaison par décimal)
+        /*
+        System.out.println("####################");
+        System.out.println("Test d'égalité f5 et f6 :");
+        System.out.println(f6.equals(f5));
+        */
+
+        //Test de comparaison (En comparant en forme de fraction, ça aurait été plus simple si on fait une comparaison par décimal)
+        /*
+        System.out.println("####################");
+        System.out.println("Test de comparaison f5 et f6 :");
+        System.out.println(f5.compareTo(f6));
+        System.out.println("Test de comparaison f2 et f5 :");
+        System.out.println(f2.compareTo(f5));
+        System.out.println("Test de comparaison f1 et f5 :");
+        System.out.println(f1.compareTo(f5));
+        */
+
+        //Test d'héritage
+        /*
+        Number aNumber = java.math.BigDecimal.ONE;
+        Number anotherNumber = new Fraction(1, 2);
+        assert java.lang.Math.abs(aNumber.doubleValue() + anotherNumber.doubleValue() - 1.5) < 1E-8;
+        */
+
+        //JEU DE TESTS (GUIDE DE COMPILATION: javac Main.java, ensuite java -ea Main)
+        //FonctionToFlottant()
+        assert (f1.FractionToFlottant() <= 0.667);
+        assert (f2.FractionToFlottant() == 5.0);
+        assert (f3.FractionToFlottant() == 0);
+        assert (f4.FractionToFlottant() <= 5.667);
+        assert (f5.FractionToFlottant() == 1.25);
+        assert (f6.FractionToFlottant() == 1.25);
+        System.out.println("Test FonctionToFlottant() OK");
+
+        //AddFraction(Fraction f)
+        assert (f1.AddFraction(f1).equals(new Fraction(4,3)));  
+        assert (f1.AddFraction(f2).equals(new Fraction(17,3)));
+        assert (f1.AddFraction(f3).equals(new Fraction(2,3)));
+        assert (f1.AddFraction(f4).equals(new Fraction(19,3)));
+        assert (f1.AddFraction(f5).equals(new Fraction(69,36)));
+        assert (f1.AddFraction(f6).equals(new Fraction(483,252)));
+        System.out.println("Test AddFonction(Fraction f) OK");
+
+        //equals(Object object) et SimplifyFraction()
+        f5.SimplifyFraction();
+        f6.SimplifyFraction();
+        assert(f5.equals(f6));
+        assert(f6.equals(f5));
+        System.out.println("Test SimplifyFraction() OK");
+        System.out.println("Test equals(Object object) OK");
+
+        //compareTo(Fraction f)
+        assert(f5.compareTo(f6) == 0);
+        assert(f2.compareTo(f5) == 1);
+        assert(f1.compareTo(f5) == -1);
+        System.out.println("TescompareTo(Fraction f) OK");      
     }
 }
